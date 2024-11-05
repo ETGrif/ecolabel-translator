2e4c550066da47e7203feedcba814d79a092d61e
mmcfeeters <mmcfeeters@mines.edu>
2024-11-05 12:12:24 -0700

    Added docstring documentation to each Manager Class

--- backend/ChatManager.py
+++ backend/ChatManager.py
@@ -1,21 +1,71 @@
 class ChatManager:
+    """
+    Manages all chats on the server
     
+    Attributes:
+        chat_storage (dict<int, list<string>>): A dictionary mapping chat_ids to the associated message history
+    """
     def __init__(self):
-        pass
+        self.chat_storage = {}
     
     def create_chat(self):
+        """
+        Creates a new chat in chatStorage, and returns the token associated with the chat
+        
+        Args: 
+            None
+        Returns:
+            token (string): A web token assigned to each chat/user. Contains a valid chat_id (int)
+        """
         raise NotImplementedError()
     
     def validate_token(self, token):
+        """
+        Ensures that the provided token is valid
+        
+        Args:
+            token (string): The web token to be validated
+            
+        Returns:
+            is_valid (bool): Whether the token is valid or not
+        """
         raise NotImplementedError()
+        
     
     def validate_exp_token(self, token):
+        """
+        Validates whether the provided token has expired or not
+        
+        Args:
+            token (string): The web token to validate
+            
+        Returns:
+            is_expired (bool): True if the token has expired. False if still valid
+        """
         raise NotImplementedError()
     
-    def get_chat(self, token):
+    def get_chat_history(self, token):
+        """
+        Given a token, returns the associated chat history
+        
+        Args:
+            token (string): The user web token
+            
+        Returns:
+            chat_history (list<string>): A chronologically ordered list of strings representing the user's chat history (includes both user and gpt responses)
+        """
         raise NotImplementedError()
     
     def terminate_chat(self, token):
+        """
+        Terminates the chat associated with the given token by removing it from the chat_storage
+        
+        Args:
+            token (string): The token representing which chat is being terminated
+            
+        Returns:
+            None
+        """
         raise NotImplementedError()
     
     
--- backend/GPTManager.py
+++ backend/GPTManager.py
@@ -1,7 +1,19 @@
 class GPTManager:
+    """
+    Handles all interactions with OpenAI/GPT
+    
+    Attributes:
+        api_key (string): A string representing the OpenAI key being used
+    """
     
     def __init__(self, api_key):
         self.api_key = api_key
     
-    def get_resp(self, prompt):
+    def get_response(self, prompt):
+        """
+        Sends the given prompt to OpenAI and retrieves the response
+        
+        Args:
+            prompt (string): The full string prompt to give to OpenAI
+        """
         raise NotImplementedError()
