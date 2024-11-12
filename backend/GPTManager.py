958056ef214fb7c6feeaafac0139ad4f3ddafea6
Mmcfeeters <112829296+Mmcfeeters@users.noreply.github.com>
2024-11-11 21:06:37 -0700

    Manager Class Documentation (#18)
    * Added docstring documentation to each Manager Class
    
    * Added type hints to each Manager Class

--- backend/GPTManager.py
+++ backend/GPTManager.py
@@ -1,7 +1,22 @@
 class GPTManager:
+    """
+    Handles all interactions with OpenAI/GPT
     
-    def __init__(self, api_key):
+    Attributes:
+        api_key (string): A string representing the OpenAI key being used
+    """
+    
+    def __init__(self, api_key: str) -> None:
         self.api_key = api_key
     
-    def get_resp(self, prompt):
+    def get_response(self, prompt: str) -> str:
+        """
+        Sends the given prompt to OpenAI and retrieves the response
+        
+        Args:
+            prompt (string): The full string prompt to give to OpenAI
+
+        Returns:
+            response (string): The response from OpenAI
+        """
         raise NotImplementedError()
